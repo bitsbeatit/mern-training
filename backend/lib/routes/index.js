d78de50d2f7f02c635661850e647afb745f0098f
lizenshakya <lizenshakya@gmail.com>
2020-12-11 15:32:08 +0545

    training #4

--- backend/lib/routes/index.js
+++ backend/lib/routes/index.js
@@ -8,5 +8,11 @@
 
         const utilityRouter = require('../modules/utilities/route');
         app.use('/api/utility', utilityRouter);
+
+        const userRouter = require('../modules/users/route');
+        app.use('/api/user', userRouter);
+
+        const loginRouter = require('../modules/login/route');
+        app.use('/api/login', loginRouter);
     };
 })(module.exports);
